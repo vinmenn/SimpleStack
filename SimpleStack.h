a08fc5fb0d743122562aa511bc24a947b3275aec
vinmen <vin.men@live.it>
2014-12-17 09:23:45 +0100

    Minor revisions
    Style alingned with other libraries
    Added library version

--- SimpleStack.h
+++ SimpleStack.h
@@ -1,18 +1,39 @@
-/*
-  =======================================================
-  SimpleStack: a stack library
-	
-  Copyright (C) 2014 Vincenzo Mennella (see license.txt)
-
-  History
-    0.1.0 14/06/2014:   First public code release
-  =======================================================
-*/
+//-------------------------------------------------------------------------------------
+// SimpleStack: a stack library
+//
+// History
+//  0.1.0 14/06/2014:   First public code release
+//
+// License:
+// "MIT Open Source Software License":
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in the
+// Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//-------------------------------------------------------------------------------------
 
 #ifndef SIMPLESTACK_H
 #define SIMPLESTACK_H
+#define LIBRARY_VERSION_SIMPLESTACK_H "0.0.1"
 
-#include <Arduino.h> 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
 class SimpleStack {
 	private:
 		uint16_t _buffer_size;	// Buffer length
@@ -236,8 +257,6 @@ void SimpleStack::popFixedString(char* fixedString, uint8_t length)
 		
 		fixedString[length] = 0;
 	}
-
-	// return name; 
 }
 // Get null terminated string from stack
 void SimpleStack::popString(char* string)
